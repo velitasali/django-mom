348746584f76213f293122a2df5168c6dd2ca34b
Veli Tasali <veli.tasali@gmail.com>
2021-05-28 01:13:42 +0300

    mom: Add lookup options & fix ownership issues
    Support null values for related fields.
    
    Various improvements.

--- django_mom/management/commands/mom.py
+++ django_mom/management/commands/mom.py
@@ -1,3 +1,4 @@
+import enum
 import logging
 import re
 from os import listdir
@@ -13,8 +14,9 @@ from django.utils.module_loading import import_string
 from ...settings import MOM_FOLDER, MOM_FILE
 
 KEY_PATTERN = "([a-zA-Z0-9-_]+)"
-FULL_PATTERN = f"^{KEY_PATTERN}.{KEY_PATTERN}.{MOM_FILE}$"
-FILE_PATTERN = f"^{KEY_PATTERN}$"
+FILE_PATTERN_FULL = f"^{KEY_PATTERN}.{KEY_PATTERN}.{MOM_FILE}$"
+FILE_PATTERN_FOLDER = f"^{KEY_PATTERN}.{MOM_FILE}$"
+FILE_PATTERN_PRIVATE = f"^{KEY_PATTERN}$"
 
 logging.basicConfig(level=logging.NOTSET)
 
@@ -37,6 +39,12 @@ class MissingLookupFieldException(MOMException):
 
 class UnsupportedValueException(MOMException):
     pass
+
+
+class Ownership(enum.Enum):
+    NONE = 'none'
+    SINGLE = 'single'
+    SHARED = 'shared'
 
 
 class MOM:
@@ -158,7 +166,7 @@ class Mapper:
             main_full_path = join(mom_folder, main_file)
 
             if isfile(main_full_path):
-                matches = re.findall(FULL_PATTERN, main_file)
+                matches = re.findall(FILE_PATTERN_FULL, main_file)
                 if len(matches) < 1:
                     continue
 
@@ -168,15 +176,23 @@ class Mapper:
                     mappers.append(Mapper.load_from(mom, model, map_name, mom_folder, main_file))
             elif isdir(main_full_path):
                 model = main_file
-                if len(re.findall(FILE_PATTERN, main_file)) != 1 or model not in mom.mapping:
+                if len(re.findall(FILE_PATTERN_PRIVATE, main_file)) != 1 or model not in mom.mapping:
                     continue
 
                 for child_file in listdir(main_full_path):
-                    map_name = child_file
-                    child_full_path = join(main_full_path, map_name)
+                    child_full_path = join(main_full_path, child_file)
 
-                    if len(re.findall(FILE_PATTERN, map_name)) == 1 and isdir(child_full_path):
-                        mappers.append(Mapper.load_from(mom, model, map_name, child_full_path, mom_file))
+                    if isdir(child_full_path):
+                        map_name = child_file
+                        if len(re.findall(FILE_PATTERN_PRIVATE, map_name)) == 1:
+                            mappers.append(Mapper.load_from(mom, model, map_name, child_full_path, mom_file))
+                    elif isfile(child_full_path):
+                        matches = re.findall(FILE_PATTERN_FOLDER, child_file)
+                        if len(matches) < 1:
+                            continue
+
+                        (map_name) = matches[0]
+                        mappers.append(Mapper.load_from(mom, model, map_name, main_full_path, child_file))
 
         return mappers
 
@@ -193,16 +209,13 @@ class Mapper:
             model_class,
             lookup_fields: dict,
             fields: dict,
-            creatable: bool = True,
-            updatable: bool = True,
+            ownership: Ownership = Ownership.NONE,
             db_object: Model = None
     ) -> (bool, bool, object):
         lookup_fields = Mapper.flatten_lookup_fields(lookup_fields)
+        fields = self.streamline_fields(fields)
 
-        if creatable:
-            updatable = True
-
-        if db_object is None or not updatable:
+        if db_object is None or ownership == Ownership.NONE:
             query: QuerySet = model_class.objects.filter(**lookup_fields)
             result_count = len(query.all())
 
@@ -210,17 +223,18 @@ class Mapper:
                 self.logger.error(f"Not unique. There are more than one results for `{self.lookup_field_value}`")
                 raise NonUniqueFieldException
 
-            updating = result_count == 1
-            db_object: Model = query.get() if updating else None
+            db_object: Model = query.get() if result_count == 1 else None
+            updating = db_object is not None
         else:
             updating = True
 
-        if not creatable and not updating:
-            self.logger.debug(f"Skip, non-creatable object `{lookup_fields}`")
-            return False, False, None
-        elif not updatable and updating:
-            self.logger.debug(f"Skip, non-updatable object `{lookup_fields}`")
-            return True, False, db_object
+        if ownership == Ownership.NONE:
+            if updating:
+                self.logger.debug(f"Skip, non-updatable object `{lookup_fields}`")
+                return True, False, db_object
+            else:
+                self.logger.debug(f"Skip, non-creatable object `{lookup_fields}`")
+                return False, False, None
 
         field_diff = {}
         many2many_diff = {}
@@ -231,35 +245,25 @@ class Mapper:
             self.logger.debug(f"Creation needed for object `{lookup_fields}`")
 
         for field_name, field_values in fields.items():
-            options = None
-            possible_options = field_name.split(' ')
-            if len(possible_options) > 1:
-                field_name = possible_options[0]
-                options = possible_options[1::]
-
             related_model_class = model_class._meta.get_field(field_name).related_model
             if related_model_class is None:
-                if options is not None and "file" in options:
-                    file_path = join(self.pwd, field_values)
-                    try:
-                        field_values = Path(file_path).read_text()
-                    except Exception as exc:
-                        self.logger.error(f"Couldn't read the file '{file_path}' for {lookup_fields}")
-                        self.logger.exception(exc)
-                        exit(1)
-
                 if not updating or field_values != getattr(db_object, field_name):
                     field_diff[field_name] = field_values
             else:
                 remapper = Remapper.create_from(self.mom, related_model_class)
-                if isinstance(field_values, dict):
+                if field_values is None:
+                    if not updating or getattr(db_object, field_name) is not None:
+                        field_diff[field_name] = None
+                elif isinstance(field_values, dict):
+                    field_values = self.streamline_fields(field_values)
+
                     if remapper is None:
                         remapper = Remapper.create_custom_from(related_model_class, field_values)
 
                     child_lookup_fields = Mapper.flatten_lookup_fields(remapper.filter_lookup_fields(field_values))
                     child_result, child_changed, child_new_value = self._start_mapping(
-                        related_model_class, child_lookup_fields, field_values, remapper.creatable,
-                        remapper.updatable, getattr(db_object, field_name) if updating else None)
+                        related_model_class, child_lookup_fields, field_values, remapper.ownership,
+                        getattr(db_object, field_name) if updating else None)
 
                     if not child_result:
                         self.logger.warning(f"Skip, related field `{field_name}` not ready for `{lookup_fields}`")
@@ -273,6 +277,7 @@ class Mapper:
                     should_update = False
 
                     for child_field_values in field_values:
+                        child_field_values = self.streamline_fields(child_field_values)
                         remapper_local = Remapper.create_custom_from(
                             related_model_class, child_field_values
                         ) if remapper is None else remapper
@@ -280,8 +285,7 @@ class Mapper:
                         child_lookup_fields = Mapper.flatten_lookup_fields(
                             remapper_local.filter_lookup_fields(child_field_values))
                         child_result, child_changed, child_new_value = self._start_mapping(
-                            related_model_class, child_lookup_fields, child_field_values, remapper_local.creatable,
-                            remapper_local.updatable, )
+                            related_model_class, child_lookup_fields, child_field_values, remapper_local.ownership, )
 
                         list_of_fields.append(child_new_value)
 
@@ -292,12 +296,19 @@ class Mapper:
                             should_update = True
 
                     if len(list_of_fields) > 0 and should_update:
+                        if remapper is not None and remapper.ownership == Ownership.SINGLE and set_m2m is not None:
+                            for existing_value in set_m2m:
+                                if existing_value not in list_of_fields:
+                                    self.logger.debug(f"""Deleting a removed related field from `{field_name}` """
+                                                      f"""for `{lookup_fields}`""")
+                                    existing_value.delete()
+
                         many2many_diff[field_name] = list_of_fields
 
         if len(field_diff) > 0 or len(many2many_diff) > 0:
             self.logger.debug(f"Saving object `{lookup_fields}`")
 
-            if updating:
+            if updating and ownership != Ownership.SHARED:
                 for key, value in field_diff.items():
                     setattr(db_object, key, value)
             else:
@@ -319,40 +330,61 @@ class Mapper:
             self.loaded, _, _ = self._start_mapping(
                 self.model_class,
                 {self.lookup_field_name: self.lookup_field_value},
-                self.fields
+                self.fields,
+                Ownership.SINGLE,
             )
         return self.loaded
+
+    def streamline_fields(self, fields: dict) -> dict:
+        streamlined_fields = {}
+        for field_name, field_value in fields.items():
+            options = field_name.split(' ')
+            if len(options) > 1:
+                field_name = options[0]
+                options = options[1::]
+                if "file" in options:
+                    file_path = join(self.pwd, field_value)
+                    try:
+                        field_value = Path(file_path).read_text()
+                    except Exception as exc:
+                        self.logger.error(f"Couldn't read the file '{file_path}' for {field_name}")
+                        self.logger.exception(exc)
+                        exit(1)
+
+            streamlined_fields[field_name] = field_value
+
+        return streamlined_fields
 
 
 class Remapper:
     from_fields: list
-    creatable: bool
-    updatable: bool
+    ownership: Ownership
     full_class_name: str
     related_model_class: object
+    from_fields_optional: list = None
 
     def __init__(
             self,
             from_fields: list,
-            creatable: bool,
-            updatable: bool,
+            ownership: Ownership,
             full_class_name: str,
-            related_model_class: object
+            related_model_class: object,
+            from_fields_optional: list = None
     ) -> None:
         super().__init__()
         self.from_fields = from_fields
-        self.creatable = creatable
-        self.updatable = updatable
+        self.ownership = ownership
         self.full_class_name = full_class_name
         self.related_model_class = related_model_class
+        self.from_fields_optional = from_fields_optional
 
     @staticmethod
     def create_from(mom: MOM, related_model_class: object):
         full_class_name = Remapper.full_class_name(related_model_class)
         self_remapping: dict
         from_fields: list
-        creatable = False
-        updatable = False
+        ownership = Ownership.NONE
+        from_fields_optional: list = []
         logger = logging.getLogger(full_class_name)
 
         if full_class_name in mom.remapping:
@@ -364,26 +396,41 @@ class Remapper:
                 logger.error(f"Missing 'from' field for class <{full_class_name}>")
                 raise MissingLookupFieldException
 
-            if "create" in self_remapping:
-                creatable = self_remapping['create']
+            if 'from~' in self_remapping:
+                from_fields_optional = self_remapping['from~']
 
-            if "update" in self_remapping:
-                updatable = self_remapping['update']
+            if "ownership" in self_remapping:
+                ownership_value = self_remapping['ownership']
+                try:
+                    ownership = Ownership(ownership_value)
+                except ValueError as exc:
+                    logger.error(f"""{ownership_value} is not a possible value for `ownership`. Possible """
+                                 f"""values are `{list(map(str, Ownership))}`""")
+                    logger.exception(exc)
+                    exit(1)
         else:
             return None
 
-        return Remapper(from_fields, creatable, updatable, full_class_name, related_model_class)
+        return Remapper(from_fields, ownership, full_class_name, related_model_class, from_fields_optional)
 
     @staticmethod
     def create_custom_from(related_model_class, fields: dict):
-        return Remapper(list(fields.keys()), False, False, Remapper.full_class_name(related_model_class),
+        return Remapper(list(fields.keys()), Ownership.NONE, Remapper.full_class_name(related_model_class),
                         related_model_class)
 
     def filter_lookup_fields(self, fields: dict):
         lookup_fields: dict = {}
-        for field, value in fields.items():
-            if field in self.from_fields:
-                lookup_fields[field] = value
+        for lookup_field in self.from_fields:
+            if lookup_field in fields:
+                lookup_fields[lookup_field] = fields[lookup_field]
+            else:
+                logging.error(f"Lookup field `{lookup_field}` of `{self.full_class_name}` isn't given for `{fields}`")
+                exit(1)
+
+        if self.from_fields_optional is not None:
+            for lookup_field in self.from_fields_optional:
+                if lookup_field in fields and lookup_field not in lookup_fields:
+                    lookup_fields[lookup_field] = fields[lookup_field]
         return lookup_fields
 
     @staticmethod
